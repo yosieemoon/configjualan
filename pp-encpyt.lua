3f056febb090c058491ebd8abf0c98cf65801647
yosieemoon <yosapat4@gmail.com>
2026-04-20 17:01:47 +0700

    Update pp-encpyt.lua

--- pp-encpyt.lua
+++ pp-encpyt.lua
@@ -3,7 +3,7 @@
 -- ====================================================================
 local CONFIG = {
     -- Setting Integrasi Webhook & Bot
-    WEBHOOK_ADMIN  = "https://discordapp.com/api/webhooks/1495399173418258503/CxTAXEIb5gRpGLUfmhZy1xqJPdcE29mF5vm1vXsKZwk1c-dzI-lxwdgDEtV5wvxwg2vD",
+    WEBHOOK_ADMIN  = "https://discordapp.com/api/webhooks/1495726080017825852/bBafExMh7xiMzK6s1mLtWFJK1XxMoyROv-qIVBS5Pwq8JvI4Kz_RoZPBbb_-48oTe5mE",
     STOCK_URL      = "https://script.google.com/macros/s/AKfycbzcub41hUz-GMIHEw0Kr2zb6bxb-A-DDqIXQLf1OxSkEy99UhUtw4y1DFHMfjSg7PH3Vw/exec",
     STOCK_SECRET   = "xx",
     STOCK_INTERVAL = 20,
